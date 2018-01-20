25c694da5364be04834aa6b9322e34d714f71786
YuJhon <3291371805@qq.com>
2018-01-20 19:36:35 +0800

    Vue业余项目开始启动

---
+++ zrainbow/src/App.vue
@@ -0,0 +1,27 @@
+<!-- 模板定义 -->
+<template>
+  <div>
+      <!-- Section one Header Define :Use Mint-UI中的Header组件-->
+      
+
+      <!-- Section Two Router-view -->
+      <router-view></router-view>
+
+      <!-- Section Three Footer Define -->
+  </div>
+</template>
+
+<!-- 脚本定义 -->
+<script>
+    /*  */
+    export default {
+    
+    }
+</script>
+
+<!-- 组件样式定义 -->
+<style scoped>
+
+</style>
+
+
---
+++ zrainbow/src/app.js
@@ -0,0 +1,7 @@
+/** Step One: 导入Vue核心文件 **/
+
+/** Step Two: App.vue文件 **/
+
+/** Step Three: 使用router-vue **/
+
+/** Step Four: 将路由绑定到Vue实例对象上 **/
